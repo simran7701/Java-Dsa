6e0571663c24b763c34b07688fe27385b130f6ec
simran prajapati <simranprajapati3508@gmail.com>
2025-08-07 00:26:41 +0530

    Create practice8.java

---
+++ practice8.java
@@ -0,0 +1,20 @@
+public class practice8 {
+
+    // Function to calculate GCD
+    public static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 36;
+        int num2 = 60;
+
+        int gcd = findGCD(num1, num2);
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd);
+    }
+}
